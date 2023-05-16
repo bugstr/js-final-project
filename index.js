63ea6a76642cd6429ff783d4cbfa6e22fac6abae
Bug <jungmokang99@gmail.com>
2023-05-16 00:16:04 -0400

    Added loading state

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // API http://www.omdbapi.com/?apikey=6e4d25bd&s=fast
 
 const movieSearch = document.querySelector("#searchInput");
+const searchButton = document.getElementById("searchButton");
+const searchInput = document.getElementById("searchInput");
 
 // load movies from API
 async function searchMovies() {
   const searchQuery = document.getElementById("searchInput").value;
+
+  const resultsWrapper = document.querySelector(".results__wrapper");
+  const spinner = document.querySelector(".fa-spinner");
+
+  document.querySelector("#results").style.display = "none";
+  resultsWrapper.classList += " results__loading";
+  spinner.style.display = "block";
 
   try {
     const response = await fetch(
@@ -18,7 +27,10 @@ async function searchMovies() {
         const movieElements = movies
           .slice(0, 6)
           .map((movie) => generateMovieElement(movie));
-        displayMovies(movieElements);
+        // setTimeout(() => {
+          displayMovies(movieElements);
+        // }, 2000);
+        document.querySelector("#results").style.display = "flex";
       } else {
         displayError("No movies found.");
       }
@@ -28,6 +40,9 @@ async function searchMovies() {
   } catch (error) {
     displayError("An error occurred: " + error);
   }
+
+  resultsWrapper.classList.remove("results__loading");
+  spinner.style.display = "none";
 }
 
 function generateMovieElement(movie) {
@@ -57,9 +72,6 @@ function displayError(errorMessage) {
   const resultsContainer = document.getElementById("results");
   resultsContainer.innerHTML = `<p>${errorMessage}</p>`;
 }
-
-const searchButton = document.getElementById("searchButton");
-const searchInput = document.getElementById("searchInput");
 
 // Event listener for search button click
 searchButton.addEventListener("click", searchMovies);
